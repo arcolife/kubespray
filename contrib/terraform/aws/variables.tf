2aa35bdf3c9cda43f8c22ff2491487b078c895eb
arcolife <archit.py@gmail.com>
2021-04-22 13:50:08 +0530

    upgrade to ubuntu 20.04

--- contrib/terraform/aws/variables.tf
+++ contrib/terraform/aws/variables.tf
@@ -25,7 +25,7 @@ data "aws_ami" "distro" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-*-amd64-server-*"]
   }
 
   filter {
